61872ddde6df30ee2f582097c8e8ea1149cb8b96
arti-1507 <artitabe15@gmail.com>
2025-08-21 21:47:10 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,30 @@
+def trap_rain_water(height):
+    n = len(height)
+    if n < 3:
+        return 0
+
+    left, right = 0, n - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left < right:
+        if height[left] <= height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water += right_max - height[right]
+            right -= 1
+
+    return water
+
+
+if __name__ == "__main__":
+    height = list(map(int, input().split()))
+    result = trap_rain_water(height)
+    print(result)
