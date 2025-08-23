789111a64463ef4fa1b3bec75bb1c20fff0e6562
arti-1507 <artitabe15@gmail.com>
2025-08-23 21:01:26 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,15 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    return prefix
+
+
+if __name__ == "__main__":
+    strs = input().strip().split()
+    print(longestCommonPrefix(strs))
