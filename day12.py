b333c4c1d93843faae64e773ab2bec89f17eb76a
arti-1507 <artitabe15@gmail.com>
2025-08-26 21:46:22 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,19 @@
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping:
+            if not stack or stack[-1] != mapping[char]:
+                return False
+            stack.pop()
+        else:
+            return False
+
+    return not stack
+
+
+s = input().strip()
+print("true" if isValid(s) else "false")
